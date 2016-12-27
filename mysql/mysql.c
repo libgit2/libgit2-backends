39bd01d9e0b559b417449aa3a0c88f4348a0e0b4
Jacques Germishuys <jacquesg@striata.com>
2016-12-27 12:12:54 +0200

    correct odb backend include

--- mysql/mysql.c
+++ mysql/mysql.c
@@ -26,7 +26,7 @@
 #include <assert.h>
 #include <string.h>
 #include <git2.h>
-#include <git2/odb_backend.h>
+#include <git2/sys/odb_backend.h>
 
 /* MySQL C Api docs:
  *   http://dev.mysql.com/doc/refman/5.1/en/c-api-function-overview.html
