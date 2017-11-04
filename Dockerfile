86dca116b4aafb1410a9d79aa3b583fcc06fb8dd
toddschilling <todd.schilling@bydeluxe.com>
2017-11-04 11:40:31 -0700

    reworked elasticsearch code.  stubbed odb methods.

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,1 @@
 FROM alpine
-
-COPY CMake/         /usr/local/src/CMake
-COPY elasticsearch/ /usr/local/src/elasticsearch
-
-RUN apk add --update alpine-sdk cmake libgit2-dev
-WORKDIR /usr/local/src/elasticsearch
--- elasticsearch/elasticsearch.c
+++ elasticsearch/elasticsearch.c
@@ -31,271 +31,56 @@
 #include <git2/sys/refdb_backend.h>
 #include <git2/sys/refs.h>
 
+#define GIT2_INDEX_NAME "git2_odb"
+
 typedef struct {
 	git_odb_backend parent;
 } elasticsearch_backend;
 
-// Since elasticsearch is just a key/value store, we'll use key suffixes
-// to denote the different "fields" we want to store for an object
+int elasticsearch_backend__read_header(size_t *len_p, git_otype *type_p, git_odb_backend *_backend, const git_oid *oid){}
 
-// the type of object
-static const char *type_suffix = ":type";
+int elasticsearch_backend__read(void **data_p, size_t *len_p, git_otype *type_p, git_odb_backend *_backend, const git_oid *oid){}
 
-// store the size so we can know how big an object is
-// without needing to fetch the data down
-static const char *size_suffix = ":size";
+int elasticsearch_backend__read_prefix(git_oid *out_oid, void **data_p, size_t *len_p, git_otype *type_p, git_odb_backend *_backend,
+	const git_oid *short_oid, size_t len) {}
+	
+int elasticsearch_backend__exists(git_odb_backend *_backend, const git_oid *oid){}
+	
+int elasticsearch_backend__write(git_odb_backend *_backend, const git_oid *id, const void *data, size_t len, git_otype type){}
 
-// the raw object data
-static const char *data_suffix = ":data";
+void elasticsearch_backend__free(git_odb_backend *_backend){}
 
-static char *elasticsearch_backend__build_key(const unsigned char *id, const char *suffix, size_t *out_len)
+static int create_index()
 {
-	char *new_key;
-
-/* 	*out_len = 20+strlen(suffix);
-	new_key = malloc(*out_len);
-	if (!new_key)
-		return NULL;
-
-	memcpy(new_key, id, 20);
-	memcpy(new_key+20, suffix, strlen(suffix)); */
-
-	return new_key;
+	int result;
+	return result;
 }
 
-int elasticsearch_backend__read_header(size_t *len_p, git_otype *type_p, git_odb_backend *_backend, const git_oid *oid)
+static int init_db()
+{
+	int result;
+	return result;
+}
+
+int git_odb_backend_elasticsearch(git_odb_backend **backend_out)
 {
 	elasticsearch_backend *backend;
-	//elasticsearch_return ret = 0;
-	char *type_key, *size_key;
-	size_t type_key_len, type_value_len, size_key_len, *size_value, size_value_len;
-	uint32_t type_flags, size_flags;
-	int status;
+	int result;
 
-/* 	assert(len_p && type_p && _backend && oid);
+	result = init_db();
 
-	backend = (elasticsearch_backend *)_backend;
+	if(result == 0)
+	{
+		backend->parent.version = GIT_ODB_BACKEND_VERSION;
+		backend->parent.read = &elasticsearch_backend__read;
+		backend->parent.read_prefix = &elasticsearch_backend__read_prefix;
+		backend->parent.read_header = &elasticsearch_backend__read_header;
+		backend->parent.write = &elasticsearch_backend__write;
+		backend->parent.exists = &elasticsearch_backend__exists;
+		backend->parent.free = &elasticsearch_backend__free;
 
-	type_key = elasticsearch_backend__build_key(oid->id, type_suffix, &type_key_len);
-	if (type_key == NULL)
-		return GIT_ENOMEM;
-
-	size_key = elasticsearch_backend__build_key(oid->id, size_suffix, &size_key_len);
-	if (size_key == NULL)
-		return GIT_ENOMEM;
-
-
-	memset(type_p, 0, sizeof(type_p));
-	type_p = (git_otype *)elasticsearch_get(backend->db, type_key, type_key_len, &type_value_len, &type_flags, &ret);
-	if (type_p == NULL) {
-		status = GIT_ENOTFOUND;
-		goto read_header_cleanup;
+		*backend_out = (git_odb_backend *)backend;
 	}
 
-	memset(size_value, 0, sizeof(size_value));
-	memset(len_p, 0, sizeof(len_p));
-	size_value = (size_t *)elasticsearch_get(backend->db, size_key, size_key_len, &size_value_len, &size_flags, &ret);
-	if (size_value == NULL) {
-		if (type_p)
-			free(type_p);
-
-		status = GIT_ENOTFOUND;
-	} else {
-		*len_p = *size_value;
-		status = GIT_SUCCESS;
-	}
-
-read_header_cleanup:
-	free(type_key);
-	free(size_key); */
-	return status;
-}
-
-int elasticsearch_backend__read(void **data_p, size_t *len_p, git_otype *type_p, git_odb_backend *_backend, const git_oid *oid)
-{
-	elasticsearch_backend *backend;
-	//elasticsearch_return ret = 0;
-	char *type_key, *data_key;
-	size_t type_key_len, data_key_len, type_value_len;
-	uint32_t type_flags, data_flags;
-	int status;
-	void *type_buffer, *data_buffer;
-
-/* 	assert(data_p && len_p && type_p && _backend && oid);
-
-	backend = (elasticsearch_backend *)_backend;
-
-	type_key = elasticsearch_backend__build_key(oid->id, type_suffix, &type_key_len);
-	if (type_key == NULL)
-		return GIT_ENOMEM;
-
-	data_key = elasticsearch_backend__build_key(oid->id, data_suffix, &data_key_len);
-	if (data_key == NULL)
-		return GIT_ENOMEM;
-
-
-	type_buffer = (void *)elasticsearch_get(backend->db, type_key, type_key_len, &type_value_len, &type_flags, &ret);
-	if (type_buffer == NULL) {
-		status = GIT_ENOTFOUND;
-		goto read_cleanup;
-	}
-
-	data_buffer = (void *)elasticsearch_get(backend->db, data_key, data_key_len, len_p, &data_flags, &ret);
-	if (data_buffer == NULL && *len_p > 0) {
-
-		if (type_buffer)
-			free(type_buffer);
-
-		status = GIT_ENOTFOUND;
-	} else {
-		*type_p = *(git_otype *)type_buffer;
-		free(type_buffer);
-
-		*data_p = data_buffer;
-		status = GIT_SUCCESS;
-	}
-
-read_cleanup:
-	free(type_key);
-	free(data_key); */
-	return status;
-}
-
-int elasticsearch_backend__exists(git_odb_backend *_backend, const git_oid *oid)
-{
-	elasticsearch_backend *backend;
-	//elasticsearch_return ret = 0;
-	int found;
-	char *type_key;
-	size_t type_key_len;
-/* 
-	assert(_backend && oid);
-
-	backend = (elasticsearch_backend *)_backend;
-	found = 0;
-
-	type_key = elasticsearch_backend__build_key(oid->id, type_suffix, &type_key_len);
-	if (type_key == NULL)
-		return GIT_ENOMEM;
-
-	// use the ADD command with a value of zero length to check for the existence of a key
-	// this is because it will let us know if the key already exists
-	// and do nothing in that case.
-	// we use SET for storing the object so this zero-length value will be overwritten
-	// by the actual value we want to store
-	ret = elasticsearch_add(backend->db, type_key, type_key_len, "", 0, 0, 0);
-	if (ret == elasticsearch_DATA_EXISTS) {
-		// object exists
-		found = 1;
-	}
-
-	free(type_key); */
-	return found;
-}
-
-int elasticsearch_backend__write(git_oid *oid, git_odb_backend *_backend, const void *data, size_t len, git_otype type)
-{
-	elasticsearch_backend *backend;
-	//elasticsearch_return ret = 0;
-	char *type_key, *size_key, *data_key;
-	size_t type_key_len, size_key_len, data_key_len;
-	int status;
-/* 
-	assert(oid && _backend && data);
-
-	backend = (elasticsearch_backend *)_backend;
-
-	if ((status = git_odb_hash(oid, data, len, type)) < 0)
-		return status;
-
-	type_key = elasticsearch_backend__build_key(oid->id, type_suffix, &type_key_len);
-	if (type_key == NULL)
-		return GIT_ENOMEM;
-
-	size_key = elasticsearch_backend__build_key(oid->id, size_suffix, &size_key_len);
-	if (size_key == NULL)
-		return GIT_ENOMEM;
-
-	data_key = elasticsearch_backend__build_key(oid->id, data_suffix, &data_key_len);
-	if (data_key == NULL)
-		return GIT_ENOMEM;
-
-
-	ret = elasticsearch_set(backend->db, type_key, type_key_len, (const char *)&type, sizeof(type), 0, 0);
-	if (ret != elasticsearch_SUCCESS) {
-		status = GIT_ERROR;
-		goto write_cleanup;
-	}
-
-	ret = elasticsearch_set(backend->db, size_key, size_key_len, (const char *)&len, sizeof(len), 0, 0);
-	if (ret != elasticsearch_SUCCESS) {
-		status = GIT_ERROR;
-		goto write_cleanup;
-	}
-
-	ret = elasticsearch_set(backend->db, data_key, data_key_len, (const char *)data, len, 0, 0);
-	if (ret != elasticsearch_SUCCESS) {
-		status = GIT_ERROR;
-		goto write_cleanup;
-	}
-
-	status = GIT_SUCCESS;
-
-write_cleanup:
-	free(type_key);
-	free(size_key);
-	free(data_key); */
-	return status;
-}
-
-void elasticsearch_backend__free(git_odb_backend *_backend)
-{
-/* 	elasticsearch_backend *backend;
-	assert(_backend);
-	backend = (elasticsearch_backend *) _backend;
-
-	if (backend->db)
-		elasticsearch_free(backend->db);
-
-	free(backend); */
-}
-
-int git_odb_backend_elasticsearch(git_odb_backend **backend_out, const char *host, int port)
-{
-/* 	elasticsearch_backend *backend;
-	elasticsearch_return ret = 0;
-	uint64_t set = 1;
-
-	backend = calloc(1, sizeof (elasticsearch_backend));
-	if (backend == NULL)
-		return GIT_ENOMEM;
-
-
-	backend->db = elasticsearch_create(NULL);
-	if (backend->db == NULL)
-		goto cleanup;
-
-	ret = elasticsearch_server_add(backend->db, host, port);
-	if (ret != elasticsearch_SUCCESS)
-		goto cleanup;
-
-	// requires elasticsearch 1.3+
-	elasticsearch_behavior_set(backend->db, elasticsearch_BEHAVIOR_BINARY_PROTOCOL, set);
-
-	elasticsearch_behavior_set(backend->db, elasticsearch_BEHAVIOR_NO_BLOCK, set);
-	elasticsearch_behavior_set(backend->db, elasticsearch_BEHAVIOR_TCP_NODELAY, set);
-
-	backend->parent.read = &elasticsearch_backend__read;
-	backend->parent.read_header = &elasticsearch_backend__read_header;
-	backend->parent.write = &elasticsearch_backend__write;
-	backend->parent.exists = &elasticsearch_backend__exists;
-	backend->parent.free = &elasticsearch_backend__free;
-
-	*backend_out = (git_odb_backend *) backend;
- */
-	return 0;
-
-/* cleanup:
-	free(backend);
-	return GIT_ERROR; */
+	return result;
 }
